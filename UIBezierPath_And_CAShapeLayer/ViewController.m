7aabed7c1b85aaab4739e68778ac530218f39fa3
huangyibiao <huangyibiao@huangyibiaodeMacBook-Pro.local>
2015-12-07 17:56:26 +0800

    add codes for CAShapeLayer

--- UIBezierPath_And_CAShapeLayer/ViewController.m
+++ UIBezierPath_And_CAShapeLayer/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) CAShapeLayer *loadingLayer;
+@property (nonatomic, strong) NSTimer       *timer;
 
 @end
 
@@ -22,6 +24,74 @@
   self.automaticallyAdjustsScrollViewInsets = NO;
   self.edgesForExtendedLayout = UIRectEdgeNone;
   
+  // 如果要测试UIBezierPath，打开注释
+//  [self testBezierPath];
+  
+  // 使用CAShapeLayer与UIBezierPath画圆
+//  [self drawCircle];
+  
+  [self drawHalfCircle];
+}
+
+- (void)drawHalfCircle {
+  self.loadingLayer = [self drawCircle];
+  
+  // 这个是用于指定画笔的开始与结束点
+  self.loadingLayer.strokeStart = 0.0;
+  self.loadingLayer.strokeEnd = 0.75;
+  
+  self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1
+                                                target:self
+                                              selector:@selector(updateCircle)
+                                              userInfo:nil
+                                               repeats:YES];
+}
+
+- (void)updateCircle {
+  if (self.loadingLayer.strokeEnd > 1 && self.loadingLayer.strokeStart < 1) {
+    self.loadingLayer.strokeStart += 0.1;
+  } else if (self.loadingLayer.strokeStart == 0) {
+    self.loadingLayer.strokeEnd += 0.1;
+  }
+  
+  if (self.loadingLayer.strokeEnd == 0) {
+    self.loadingLayer.strokeStart = 0;
+  }
+  
+  if (self.loadingLayer.strokeStart >= 1 && self.loadingLayer.strokeEnd >= 1) {
+    self.loadingLayer.strokeStart = 0;
+    [self.timer invalidate];
+    self.timer = nil;
+  }
+}
+
+- (CAShapeLayer *)drawCircle {
+  CAShapeLayer *circleLayer = [CAShapeLayer layer];
+  // 指定frame，只是为了设置宽度和高度
+  circleLayer.frame = CGRectMake(0, 0, 200, 200);
+  // 设置居中显示
+  circleLayer.position = self.view.center;
+  // 设置填充颜色
+  circleLayer.fillColor = [UIColor clearColor].CGColor;
+  // 设置线宽
+  circleLayer.lineWidth = 2.0;
+  // 设置线的颜色
+  circleLayer.strokeColor = [UIColor redColor].CGColor;
+  
+  // 使用UIBezierPath创建路径
+  CGRect frame = CGRectMake(0, 0, 200, 200);
+  UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:frame];
+  
+  // 设置CAShapeLayer与UIBezierPath关联
+  circleLayer.path = circlePath.CGPath;
+  
+  // 将CAShaperLayer放到某个层上显示
+  [self.view.layer addSublayer:circleLayer];
+  
+  return circleLayer;
+}
+
+- (void)testBezierPath {
   CGFloat height = [UIScreen mainScreen].bounds.size.height;
   BezierPathView *v = [[BezierPathView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, height - 140 - 64)];
   [self.view addSubview:v];
@@ -29,18 +99,17 @@
   v.layer.borderWidth = 5;
   v.backgroundColor = [UIColor whiteColor];
   
-//  v.type = kDefaultPath;
-//  v.type = kRectPath;
-//  v.type = kCirclePath;
-//  v.type = kOvalPath;
-//  v.type = kRoundedRectPath;
-//  v.type = kArcPath;
-//  v.type = kSecondBezierPath;
+  //  v.type = kDefaultPath;
+  //  v.type = kRectPath;
+  //  v.type = kCirclePath;
+  //  v.type = kOvalPath;
+  //  v.type = kRoundedRectPath;
+  //  v.type = kArcPath;
+  //  v.type = kSecondBezierPath;
   v.type = kThirdBezierPath;
   
   self.view.layer.borderWidth = 1;
   self.view.layer.borderColor = [UIColor blueColor].CGColor;
 }
-
 
 @end
